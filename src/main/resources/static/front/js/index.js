ccfe4dec1f5bf55abaa11d7734cbb72df15f1ecd
yiyeduyin <cgnor03@gmail.com>
2016-02-11 15:37:45 +0800

    更新前端页面

--- src/main/resources/static/front/js/index.js
+++ src/main/resources/static/front/js/index.js
@@ -1,6 +1,43 @@
 var app = angular.module("frontApp", []);
-app.controller('index',function($rootScope, $scope, $http, $location) {
 
-    
+//获取产品类别
+app.service('getProductTypeList', function($rootScope, $http) {
+    if(!$rootScope.productTypeList){
+		$http.get('/rest/productType/list', {}).success(function(res) {
+            if (res.errcode == 0) {
+                $rootScope.productTypeList = res.data;
+            }
+        });
+	}
 });
 
+
+app.controller('index', function($rootScope, $scope, $http, $location, getProductTypeList) {
+
+
+});
+
+app.controller('about', function($rootScope, $scope, $http, $location, getProductTypeList) {
+
+
+});
+
+app.controller('certificates', function($rootScope, $scope, $http, $location, getProductTypeList) {
+
+
+});
+
+app.controller('engineerings', function($rootScope, $scope, $http, $location, getProductTypeList) {
+
+
+});
+
+app.controller('products', function($rootScope, $scope, $http, $location, getProductTypeList) {
+
+
+});
+
+app.controller('profile', function($rootScope, $scope, $http, $location, getProductTypeList) {
+
+
+});
